09d8a76cf1fd44466396c8cefeabefc52cb5cea3
skamalj <skamalj>
2023-09-21 06:23:29 +0000

    Spectrum tested

--- notebooks/AWSNotebook.py
+++ notebooks/AWSNotebook.py
@@ -26,3 +26,31 @@
 
 # MAGIC %sql
 # MAGIC drop  table main.default.flights;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC insert into main.default.test  select * from main.default.flights
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC GENERATE symlink_format_manifest FOR TABLE main.default.test
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC ALTER TABLE main.default.test SET TBLPROPERTIES(delta.compatibility.symlinkFormatManifest.enabled=true)
+
+# COMMAND ----------
+
+create external schema myspectrum_schema 
+from data catalog 
+database 'myspectrum_db' 
+iam_role 'arn:aws:iam::<account>:role/service-role/AmazonRedshift-CommandsAccessRole-20220901T162738'
+create external database if not exists;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count(*) from main.default.test;
